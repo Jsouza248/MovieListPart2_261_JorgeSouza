ae9ccce7656260eea80bfd78c6a93633c35cc5be
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-03-03 23:15:25 -0800

    Movie Guide Part 2 261 Jorge Souza

--- MovieListPart2_261_JorgeSouza.py
+++ MovieListPart2_261_JorgeSouza.py
@@ -1,2 +1,68 @@
-#Jorge Souza, 261, Movie Guide Part 2
+#Jorge Souza 
+#261 Movie 
+# Guide Part 2
 
+FILENAME = "Movies.txt"
+
+def write_movies(movies):
+    with open(FILENAME, "w") as file:
+        for movie in movies:
+            file.write(f"{movie}\n")
+
+def read_movies():
+    movies = []
+    with open(FILENAME) as file:
+        for line in file:
+            line = line.replace("\n", "")
+            movies.append(line)
+    return movies
+
+def list_movies(movies):
+    for i, movie in enumerate(movies, start=1):
+        print(f"{i}. {movie}")
+    print()
+
+def add_movie(movies):
+    movie = input("Movie: ")
+    movies.append(movie)
+    write_movies(movies)
+    print(f"{movie} was added.\n")
+
+def delete_movie(movies):
+    index = int(input("Number: "))
+    if index < 1 or index > len(movies):
+        print("Invalid movie number.\n")
+    else:
+        movie = movies.pop(index - 1)
+        write_movies(movies)
+        print(f"{movie} was deleted.\n")
+
+def display_menu():
+    print("The Movie List Program")
+    print()
+    print("COMMAND MENU")
+    print("list - List All Movies")
+    print("add - Add A Movie")
+    print("del - Delete A Movie")
+    print("exit - Exit Program")
+    print()
+
+def main():
+    display_menu()
+    movies = read_movies()
+    while True:
+        command = input("Command: ")
+        if command.lower() == "list":
+            list_movies(movies)
+        elif command.lower() == "add":
+            add_movie(movies)
+        elif command.lower() == "del":
+            delete_movie(movies)
+        elif command.lower() == "exit":
+            print("BYE")
+            break
+        else:
+            print("NOT A VALID COMMAND. Please try again.")
+
+if __name__ == "__main__":
+    main()
